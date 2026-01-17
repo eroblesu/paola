929d3feb543cd2cd24921644632e400a38cfe4d9
eroblesu <eduardo.robles.unzueta@alumnos.upm.es>
2026-01-17 20:42:38 +0100

    feat: recover copyright

--- src/config/navData.json.ts
+++ src/config/navData.json.ts
@@ -29,27 +29,6 @@ const navConfig: navItem[] = [
 		text: "Contacto",
 		link: "/#contact",
 	},
-	// {
-	// 	text: "Pages",
-	// 	dropdown: [
-	// 		{
-	// 			text: "Portfolio",
-	// 			link: "/portfolio/couple-1/",
-	// 		},
-	// 		{
-	// 			text: "Legal",
-	// 			link: "/privacy-policy/",
-	// 		},
-	// 		{
-	// 			text: "Elements",
-	// 			link: "/elements/",
-	// 		},
-	// 		{
-	// 			text: "404",
-	// 			link: "/not-a-link/",
-	// 		},
-	// 	],
-	// },
 ];
 
 export default navConfig;
